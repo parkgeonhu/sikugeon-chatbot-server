7fabf809f8e9de3b03226ee58fe7d563ca7b068c
parkgeonhu <parkgeonhu@gmail.com>
2020-03-26 17:22:42 +0000

    Add location response

--- app/api/views.py
+++ app/api/views.py
@@ -1,12 +1,36 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.http import JsonResponse
 from django.shortcuts import render
+import json
 from django.views.decorators.csrf import csrf_exempt
 
+
 # Create your views here.
+
 @csrf_exempt
 def reply(request):
     print(request.body.decode('utf-8'))
-    result = {"test": "No order received"}
-    return JsonResponse(result, status=200)
     
+    response=json.loads(request.body)
+    
+    location=response["action"]["params"]["location"]
+    
+    
+    
+    result = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": location
+                    }
+                }
+            ]
+        }
+    }
+
+
+    return JsonResponse(result, status=200)
