c21c12db0044d41a3b51b22768e223ddfd095036
parkgeonhu <parkgeonhu@gmail.com>
2020-03-28 15:29:52 +0000

    Add parser.py for parsing on kakao map

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import reply
+from .views import reply, dataParsing
 
 urlpatterns = [
     path('reply/', reply),
+    path('parsing/', dataParsing),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -5,9 +5,32 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .parser import get_sikugeon_list
 
 
 # Create your views here.
+@csrf_exempt
+def dataParsing(request):
+    print(request.body.decode('utf-8'))
+    
+    # response=json.loads(request.body)
+    
+    places=get_sikugeon_list()
+    
+    result = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "파싱 완료"
+                    }
+                }
+            ]
+        }
+    }
+
+    return JsonResponse(result, status=200)
 
 @csrf_exempt
 def reply(request):
